172f5a64dff73c5eacf7f78d7940bee4f2b428c7
Your Name <you@example.com>
2019-12-06 15:03:46 -0500

    added Naive Search Problem

---
+++ naivesearch/naiveSearch.js
@@ -0,0 +1,13 @@
+// Naive String Search
+// Loop over the longer string
+// Loop over the shorter string
+// If the characters don't match, break out of the inner loop
+// If the characters do match, keep going
+// If you complete the inner loop and find a match, increment the count of matches
+// Return the count
+
+
+function nativeSearch() {
+
+
+}
