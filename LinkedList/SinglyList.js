e4bb633441e27aeb75b1b959a7e9111f354e6baa
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-13 16:42:19 -0500

    Added Push method to SinglyList Class

--- LinkedList/SinglyList.js
+++ LinkedList/SinglyList.js
@@ -11,9 +11,20 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        let newNode = new Node(val);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = this.head;
 
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 
-
-
-
+}
+let list = new SinglyLinkedList()
+list.push("Hello")
