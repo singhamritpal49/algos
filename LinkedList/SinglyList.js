df3fe7c78938f2d6cb5dd662fba1546cbf34dafe
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-13 16:41:17 -0500

    Singly LinkedList initilized with node val,next,head,tail,length

--- LinkedList/SinglyList.js
+++ LinkedList/SinglyList.js
@@ -1,12 +1,15 @@
 class Node {
     constructor(val) {
-        
+        this.val = val;
+        this.next = null;
     }
 }
 
 class SinglyLinkedList {
     constructor() {
-        
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
     }
