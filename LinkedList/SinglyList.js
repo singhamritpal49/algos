50ace39d03e210d48509289ad2aa51ab8b06f513
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-15 01:03:46 -0500

    added method set()

--- LinkedList/SinglyList.js
+++ LinkedList/SinglyList.js
@@ -68,7 +68,14 @@ class SinglyLinkedList {
         return temp;
     }
 
-   
+    set(index,val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true
+        }
+        return false
+    }
 
 
 
