5416db0bd840bcc8b194fe13b45a272548303df0
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-18 00:11:34 -0500

    added insert method to doubly list

--- LinkedList/DoublyList.js
+++ LinkedList/DoublyList.js
@@ -124,6 +124,25 @@ class DoublyLinkedList {
             return false
         }
     }
+    insert(index, val) {
+        if (index < 0 || index > this.length)
+            return false;
+        if (index === 0) {
+            return this.unshift(val)
+        }
+        if (index === this.length) {
+            return this.push(val)
+        }
+        let newNode = new Node(val);
+        let beforeNode = this.get(index - 1);
+        let afterNode = beforeNode.next;
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode
+        newNode.next = afterNode;
+        afterNode.prev = newNode
+        this.length++;
+        return this;
 
+    }
 
 }
