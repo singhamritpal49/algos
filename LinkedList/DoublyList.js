4af207c5086dac11a4696d838552cdaa79dd0884
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-18 00:08:55 -0500

    added shift method to doubly list

--- LinkedList/DoublyList.js
+++ LinkedList/DoublyList.js
@@ -40,5 +40,21 @@ class DoublyLinkedList {
         this.length--;
         return currentTail;
     }
+    shift() {
+        let oldHead = this.head
+        if (this.length === 0)
+            return undefined;
+        else if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+
+    }
 
 }
