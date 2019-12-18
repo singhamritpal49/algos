27581fffdcfe861fb7749626d589393b375955f2
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-18 00:09:27 -0500

    added unshift method to doubly list

--- LinkedList/DoublyList.js
+++ LinkedList/DoublyList.js
@@ -56,5 +56,19 @@ class DoublyLinkedList {
         return oldHead;
 
     }
+    unshift(val) {
+        let newNode = new Node(val);
+
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++
+        return this
+    }
 
 }
