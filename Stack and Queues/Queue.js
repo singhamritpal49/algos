c24fd0075b330a49a771fa190a92c4829820e944
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-19 01:28:43 -0500

    Added enqueue method to Queues

--- Stack and Queues/Queue.js
+++ Stack and Queues/Queue.js
@@ -11,4 +11,16 @@ class Queue {
         this.last = null;
         this.size = 0;
     }
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        this.size++
+        return this
+    }
 }
