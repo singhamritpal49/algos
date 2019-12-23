0a9842ce66a9973d6b9fbe8ad10707918e68e4c1
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-23 00:18:51 -0500

    added Binary Search Tree Class

---
+++ Binary Search Trees/binarysearchtree.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null; 
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+}
+
+
+
+let tree = new BinarySearchTree();
