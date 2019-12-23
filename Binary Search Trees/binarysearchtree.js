d3870724de18450c62190dc00271a23d51c5f9da
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-23 03:21:30 -0500

    Added insert method to binary

--- Binary Search Trees/binarysearchtree.js
+++ Binary Search Trees/binarysearchtree.js
@@ -11,6 +11,34 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(value) {
+        let newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (value === current.value) return undefined;
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left
+                    }
+                }
+                else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
 
 }
 
