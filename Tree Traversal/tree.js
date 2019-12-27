405ef9a4c9de783d06c83e4ecfd156bcc7295973
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-27 00:06:20 -0500

    Added BFS Method to tree

---
+++ Tree Traversal/tree.js
@@ -0,0 +1,95 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+   
+
+    BFS() {
+        let currentNode = this.root;
+        let queue = [];
+        let data = [];
+            queue.push(currentNode)
+        while(queue.length) {
+            currentNode = queue.shift();
+            data.push(currentNode.value);
+            if(currentNode.left) queue.push(currentNode.left)
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+        return data
+    }
+}
+
+
+var tree = new Tree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
+    
+
