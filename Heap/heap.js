9e1f3926b0454eed3bccb37786bfdd5315d27d72
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-09 15:51:30 -0500

    added heap

--- Heap/heap.js
+++ Heap/heap.js
@@ -1,5 +1,6 @@
 class MaxBinaryHeap {
     constructor() {
         this.values = [40, 39, 33, 18, 27, 12];
+      
     }
 }
---
+++ Other/FindEven.js
@@ -0,0 +1,19 @@
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation:
+// 12 contains 2 digits (even number of digits).
+// 345 contains 3 digits (odd number of digits).
+// 2 contains 1 digit (odd number of digits).
+// 6 contains 1 digit (odd number of digits).
+// 7896 contains 4 digits (even number of digits).
+// Therefore only 12 and 7896 contain an even number of digits.
+
+
+var findNumbers = function(nums) {
+    let count = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i].toString().length % 2 === 0)
+            count++
+    }
+    return count
+};
