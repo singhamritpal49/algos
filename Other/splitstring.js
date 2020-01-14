8eff104047fd94e16707aea27baa3788dbfb98a2
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-13 23:28:38 -0500

    Solved Split String

--- Other/splitstring.js
+++ Other/splitstring.js
@@ -1,5 +1,13 @@
 function solution(str) {
     // solution('abc') // should return ['ab', 'c_']
     // solution('abcdef') // should return ['ab', 'cd', 'ef']
-    
+    let result = []
+    for (let i = 0; i < str.length;  i= i + 2) {
+        if (str.length % 2 === 0) {
+            result.push(`${str[i] + str[i+1]}`)
+        } else {
+            result.push(str[i] + ( str[i+1] ? str[i+1] : "_" ))
+        }
+    }
+    return result
 }
