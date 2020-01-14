eb4f2a001023ba28fea03932f682a80e1ccaa521
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-13 23:32:50 -0500

    solved getmiddle

--- Other/getMiddle.js
+++ Other/getMiddle.js
@@ -1,4 +1,13 @@
 function getMiddle(string) {
     // ("test") should return "es"
     //   "testing") should return "t" 
+    let result
+    if (string.length % 2 === 0) {
+        result = (string[Math.floor(string.length / 2) - 1])
+        + (string[Math.floor(string.length / 2)])
+    } else {
+        result = (string[Math.floor(string.length / 2)])
+    }
+    return result
+
 }
