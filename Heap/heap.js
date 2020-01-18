051801d813b0191b63701d959f38093672ec320a
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-18 16:16:54 -0500

    added heap method insert

--- Heap/heap.js
+++ Heap/heap.js
@@ -3,4 +3,10 @@ class MaxBinaryHeap {
         this.values = [40, 39, 33, 18, 27, 12];
       
     }
+
+    insert(element) {
+        this.values.push(element)
+        this.bubbleUp()
+
+    }
 }
