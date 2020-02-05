73695bfc7842b1fbc227c8bff4acea5cf7d6cd1b
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-05 16:07:01 -0500

    added charcode problem

---
+++ Other/charCode.js
@@ -0,0 +1,9 @@
+function values(string) {
+    // a = 1, b = 2, c = 3, .... z = 26.
+    let count = 0
+    for(let i = 0; i < string.length; i++) {
+    let value = string[i].charCodeAt() - 96
+    count += value
+}
+return value 
+}
