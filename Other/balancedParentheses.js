a2321ea4aa715077a3e645df351c77ea96dc89b6
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-15 01:48:27 -0500

    balancing parentheses

---
+++ Other/balancedParentheses.js
@@ -0,0 +1,27 @@
+function balance(string) {
+
+    let hash = {};
+    for(let i = 0; i < string.length; i++) {
+        hash[string[i]] = (hash[string[i]] || 0) + 1
+    }
+        const values = Object.values(hash)
+    
+    for(let i = 0; i <values.length;i++) {
+        if (values[i] > values[i+1]) {
+               let value = values[i] - values[i+1]
+               return value
+        } 
+        else if (values[i+1] > values[i]) {
+               let value = values[i+1] - values[i]
+               return value
+        } else if(values[i] = values[i+1]) {
+            return 0
+        }
+
+
+    }
+
+
+}
+
+console.log(balance("())()"))
