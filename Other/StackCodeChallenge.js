81c52061c14245f1a186f4fbb35491cfd54be032
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-18 20:04:17 -0500

    Added code challenge Stack

---
+++ Other/StackCodeChallenge.js
@@ -0,0 +1,23 @@
+class Stack {
+    
+      // setup some built in data type
+      // holds the stack
+
+    push( value ) {
+      // Put value onto the stack
+    }
+    pop() {
+      // remove the the value at the top of the stack
+      // and return it
+    }
+    peek() {
+      // return the value at the top of the stck
+      // but do not remove it from the stack
+
+    }
+    
+  }
+  let stack = new Stack()
+  stack.push(4)
+  stack.push(6)
+  stack.push(1)
