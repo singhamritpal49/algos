84bf7cd80747383fd8b4a3e5dace68186a935e83
Amritpal Singh <singhamritpal49@gmail.com>
2020-03-13 23:29:39 -0400

    added domain problem

---
+++ Other/Extract-the-domain-name-from-a-URL.js
@@ -0,0 +1,15 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+
+
+function domainName(url){
+    //your code here
+  }
+
+//   Test.assertEquals(domainName("http://google.com"), "google");
+// Test.assertEquals(domainName("http://google.co.jp"), "google");
+// Test.assertEquals(domainName("www.xakep.ru"), "xakep");
+// Test.assertEquals(domainName("https://youtube.com"), "youtube");
