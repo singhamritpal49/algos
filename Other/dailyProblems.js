97cb636b91f910a92faa8bf700936b017b63aea3
Amritpal Singh <singhamritpal49@gmail.com>
2020-04-25 21:58:52 -0400

    moveZeros

--- Other/dailyProblems.js
+++ Other/dailyProblems.js
@@ -13,3 +13,33 @@ function problemOne(array,num) {
     }
 return false;
 }
+
+
+
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Example:
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+
+
+function moveZeroes(nums) {
+    let array = []
+for(let i = 0; i < nums.length;i++) {
+    if(nums[i] === 0) {
+        array.push(nums[i])
+        nums.splice(i,1)
+    }   
+}
+
+for(let i = 0; i <= array.length;i++) {
+    let first = array.pop()
+        nums.push(first)
+}
+return nums;
+}
