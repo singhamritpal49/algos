0f66965fc708ee6741d4bfede6b195ef4f42439b
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-01 19:08:39 -0400

    solved function capitalize

--- Other/capitalization.js
+++ Other/capitalization.js
@@ -3,5 +3,10 @@
 // capitalize(‘lazy days during quarantine’) →” Lazy Days During Quarantine”
 
 function capitalize(string) {
- 
+    let words = string.split(' '); 
+     string = []
+    for(let i = 0; i < words.length;i++) {
+            string.push(words[i][0].toUpperCase() + words[i].slice(1))
+    }
+    return string.join(' ')
 }
