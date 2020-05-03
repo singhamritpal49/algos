18e065f47697dc87c44bb584baec8f51bf39b708
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-02 20:11:16 -0400

    solved palindrome problem

--- Other/Palindrome.js
+++ Other/Palindrome.js
@@ -3,14 +3,27 @@
 
 // Implement a function that checks if something is a palindrome.
 
-Examples
-isPalindrome("anna")   ==> true
-isPalindrome("walter") ==> false
-isPalindrome(12321)    ==> true
-isPalindrome(123456)   ==> false
+// Examples
+// isPalindrome("anna")   ==> true
+// isPalindrome("walter") ==> false
+// isPalindrome(12321)    ==> true
+// isPalindrome(123456)   ==> false
 
 
 function isPalindrome(line) {
-
+    let newLine = line.toString().split('');
+    let length = newLine.length-1;
+    let result = true;
+    for (let i = 0; i < newLine.length; i++) {
+            if(newLine[i] === newLine[length]) {
+                
+                result = true
+            }
+            if(newLine[i] != newLine[length]) {
+                result = false
+            }
+            length--
+     }
+return result;
 }
 
