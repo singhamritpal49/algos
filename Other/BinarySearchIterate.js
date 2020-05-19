a38397cbfaf62dd89baf0b11af2d2f57cf37fa80
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-18 23:00:46 -0400

    Initilized binarySearch

--- Other/BinarySearchIterate.js
+++ Other/BinarySearchIterate.js
@@ -17,3 +17,7 @@
 // If x matches with middle element, we return the mid index.
 // Else If x is greater than the mid element, then x can only lie in right half subarray after the mid element. So we recur for right half.
 // Else (x is smaller) recur for the left half.
+
+function binarySearch(array,item) {
+    
+    }
