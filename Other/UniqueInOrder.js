c87894b3a1d7a54e8fca834d9c961ff72e5c6b11
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-21 14:25:13 -0400

    added uniqueInOrder alt solution

--- Other/UniqueInOrder.js
+++ Other/UniqueInOrder.js
@@ -12,4 +12,29 @@ var uniqueInOrder=function(iterable){
     }
       return iterable
   }
+
+
+//   Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
   
+
+// Another Solution
+
+function uniqueInOrder(iterable) {
+    let array = [];
+    
+    for(let i = 0; i < iterable.length;i++) {
+      let lastElement = array[array.length - 1];
+      if(lastElement !== iterable[i]) {
+          array.push(iterable[i])
+      }
+    }
+    
+      return array;
+      
+    }
