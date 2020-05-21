c37d20c320c9a7159f12b332eee6bad4f81dd8da
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-20 22:45:59 -0400

    added descripition kababize

--- Other/kababize.js
+++ Other/kababize.js
@@ -1,3 +1,9 @@
+// Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+// kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+// kebabize('camelsHave3Humps') // camels-have-humps
+
+
 function kababize(str) {
     let array = [];
     for (let i = 0; i < str.length; i++) {
