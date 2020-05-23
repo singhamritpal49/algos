012a4c4b2b6d51fb6e9672e72f8d553039aebdc2
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-23 15:21:32 -0400

    Solved highest rank

--- Other/highestRask.js
+++ Other/highestRask.js
@@ -8,3 +8,18 @@
 // [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
 
 
+function highestRank(arr) {
+    let hashMap = {};
+    for (let i = 0; i < arr.length; i++) {
+        hashMap[arr[i]] = (hashMap[arr[i]] || 0) + 1
+    }
+    let valueToReturn = 0;
+    let keyToReturn = 0;
+    for (let key in hashMap) {
+        if (hashMap[key] >= valueToReturn) {
+            valueToReturn = hashMap[key];
+            keyToReturn = key;
+        }
+    }
+    return parseInt(keyToReturn);
+}
