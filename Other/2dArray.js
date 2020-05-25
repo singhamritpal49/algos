d7589c8aea6140aeaf87975fa1b66a122aaab8b5
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-24 22:21:22 -0400

    added countNumbers

---
+++ Other/2dArray.js
@@ -0,0 +1,21 @@
+// When no more interesting kata can be resolved, I just choose to create the new kata, to solve their own, to enjoy the process --myjinxin2015 said
+
+// Description:
+// Give an 2D integer array arr that contains some integers. Array arr has the following properties:
+
+// Integers in each row are sorted from left to right.
+
+// Integers in each column are sorted from up to bottom.
+
+// No duplicate integers in each row or column. For example:
+
+// arr = [
+// [1, 3, 5, 7],
+// [2, 4, 7, 8],
+// [3, 5, 9, 10]
+// ]
+
+
+function countNumber(array, num) {
+   
+}
