b61684763400279a6d6e4eeadc0cbc57e559f2cf
Amritpal Singh <singhamritpal49@gmail.com>
2020-05-30 18:02:05 -0400

    letter changes alt

--- Other/LetterCharacter.js
+++ Other/LetterCharacter.js
@@ -29,3 +29,26 @@ function LetterChanges(str) {
 
 
 }
+
+
+
+function LetterChanges(str) {
+    str = str.split('')
+    let string = "abcdefghijklmnopqrtuwzyza"
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < string.length; j++) {
+            if (str[i] === string[j]) {
+                str[i] = string[j + 1]
+                break;
+            }
+
+        }
+        if (str[i] === "a" || str[i] === "e" || str[i] === 'i' || str[i] === 'o' || str[i] === "u") {
+            str[i] = str[i].toUpperCase()
+        }
+    }
+    // code goes here
+    return str.join('');
+
+}
